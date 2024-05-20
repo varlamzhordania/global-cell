73b8399ad74aeff490d945fea93c31d9b30fd77d
AmirBigdeli <141164942+varlamzhordania@users.noreply.github.com>
2024-05-20 21:10:30 +0400

    Ready for deployment

--- web/core/settings/settings.py
+++ web/core/settings/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'estdfuyser5setsetgf')
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'estdfuyser5setsetgf')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv(DJANGO_DEBUG,True)
 
 ALLOWED_HOSTS = []
 
