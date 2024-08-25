ed5b13a0003a9e4a6c2f5a676de6c68d1b002646
AmirBigdeli <141164942+varlamzhordania@users.noreply.github.com>
2024-08-25 05:39:58 +0400

    nc

--- web/main/context_processors.py
+++ web/main/context_processors.py
@@ -8,9 +8,11 @@ def Default(request):
         notifications = Notification.objects.filter(user=request.user, is_active=True, has_seen=False)
         return {
             'unseen_notifications': notifications,
-            'asset_version': settings.STATIC_VERSION
+            'asset_version': settings.STATIC_VERSION,
+            'debug': settings.DEBUG
         }
     else:
         return {
-            'asset_version': settings.STATIC_VERSION
+            'asset_version': settings.STATIC_VERSION,
+            'debug': settings.DEBUG
         }
